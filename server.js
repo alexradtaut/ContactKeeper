1d7dcea1998d50c6a4392fc5c8bbf65933285cc0
aradtaut <a.rad@redsharp.ro>
2020-05-05 09:24:42 +0300

    Deploy

--- server.js
+++ server.js
@@ -18,14 +18,11 @@ app.use("/api/contacts", require("./routes/contacts"));
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
-//build mode
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
-});
 
 const PORT = process.env.PORT || 5000;
 
